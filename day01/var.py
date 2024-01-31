130bbfed976bdbc788849f47a84c24e8e7e648e0
AidenKpark <152140839+AidenKpark@users.noreply.github.com>
2024-01-31 10:48:51 +0900

    Add files via upload

---
+++ day01/var.py
@@ -0,0 +1,11 @@
+# variable - 변수
+# myname = input("너의 이름은?")
+# age = 10 + 20
+# print(myname)
+# print(age)
+
+name = input("당신의 이름은?")
+age = input("당신의 나이는?")
+food = input("당신의 최애 음식은?")
+youtuber = input("당신의 최애 유튜버는?")
+print(name, age, food, youtuber)
